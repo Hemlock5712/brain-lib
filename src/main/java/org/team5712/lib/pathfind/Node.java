5a4c06369d5c010cc97ca58e9721fd46dd99bc80
JosephTLockwood <joeojazz@gmail.com>
2023-01-07 20:43:54 -0500

    Add getX getY and holo support

--- src/main/java/org/team5712/lib/pathfind/Node.java
+++ src/main/java/org/team5712/lib/pathfind/Node.java
@@ -8,23 +8,32 @@ import edu.wpi.first.math.geometry.Rotation2d;
 public class Node {
     double x, y;
     Rotation2d holonomicRotation;
-    List<Node> neighbors;
-
+    List < Node > neighbors;
+  
     public Node(double x, double y) {
         this.x = x;
         this.y = y;
         holonomicRotation = Rotation2d.fromDegrees(0);
-        this.neighbors = new ArrayList<>();
+        this.neighbors = new ArrayList < > ();
     }
-
+  
     public Node(double x, double y, Rotation2d holonomicRotation) {
         this.x = x;
         this.y = y;
         this.holonomicRotation = holonomicRotation;
-        this.neighbors = new ArrayList<>();
+        this.neighbors = new ArrayList < > ();
     }
-
+  
     public void addNeighbor(Node neighbor) {
         this.neighbors.add(neighbor);
     }
-}
+    public double getX(){
+      return x;
+    }
+    public double getY(){
+      return y;
+    }
+    public Rotation2d getHolRot(){
+      return holonomicRotation;
+    }
+  }
