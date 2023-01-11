89630da689e88eefd597d74aac04911a7a7de5cd
JosephTLockwood <joeojazz@gmail.com>
2023-01-11 09:51:24 -0500

    Change obstacleOffset to double

--- src/main/java/org/team5712/lib/util/Doubles.java
+++ src/main/java/org/team5712/lib/util/Doubles.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class Doubles {
     public static double[] toArray(List<Double> arr) {
-        double[] floats = new double[arr.size()];
+        double[] doubles = new double[arr.size()];
         for(int i = 0; i < arr.size(); i++) {
-            floats[i] = arr.get(i);
+            doubles[i] = arr.get(i);
         }
-        return floats;
+        return doubles;
     }
 }
